f9d50a055efadf9612d854269706af6738fb2d22
Martina Garcia <martinagg@myuax.com>
2022-11-15 13:21:09 +0100

    mar15

---
+++ entrega1.py
@@ -0,0 +1,34 @@
+28/2**5 
+n(_) #aproximación
+
+sqrt(2) #raíz
+n(_)
+sqrt(-9)
+log(2153693963075557766310747,3)
+N(e,digits=50) #50primerosdigitosdee
+pi
+N(pi,digits=80) 
+956//98 #division
+956%98  #resto
+9*98+74 #prueba de euclides
+def divisor_de_234878(n):   #definiciondivisor
+    return 234878 % n ==0
+
+divisor_de_234878(3)
+    #divisores
+divisors(1002)
+divisors(422)
+divisors(491)
+divisors(134)
+    #factores
+134.factor()
+491.factor()
+1002.factor()
+422.factor()
+    #mcm mcd
+(2).gcd(5)
+(2).lcm(5)
+(4).gcd(10)
+(4).lcm(10)
+(18).gcd(51)
+(18).lcm(51)
